c63c56bb4fd517ba0963a7a2e33199bb0411e1e0
Pontus Stenetorp <pontus@stenetorp.se>
2014-05-06 18:20:56 +0900

    Tools for dependency data

---
+++ GNUmakefile
@@ -0,0 +1,22 @@
+# Makefile for Allén.
+#
+# Author:	Pontus Stenetorp	<pontus stenetorp se>
+# Version:	2014-05-06
+
+BUILD=build
+MALT_VER=1.8
+MALT_URL=http://maltparser.org/dist/maltparser-${MALT_VER}.tar.gz
+FETCH_CMD=wget
+
+${BUILD}:
+	mkdir ${BUILD}
+
+${BUILD}/maltparser: ${BUILD}
+	${FETCH_CMD} -P build "${MALT_URL}"
+	tar -x -z -f "${@}-${MALT_VER}.tar.gz" -C build
+	cd build && ln -s -f "maltparser-${MALT_VER}" maltparser
+	cd "${@}" && ln -s -f "maltparser-${MALT_VER}.jar" maltparser.jar
+
+.PHONY: clean
+clean:
+	rm -r -f ${BUILD}
---
+++ tools/proj.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Projectivise dependency data using the MaltParser.
+#
+# Example:
+#
+#   for F in `ls */{train,dtrain}.conll`; do B=`echo "${F}" \
+#       | sed -e 's|\.conll$||'`; cat "${F}" \
+#       | ./proj.sh > "${B}_proj.conll"; done;
+#
+# Author:   Pontus Stenetorp    <pontus stenetorp se>
+# Version:  2013-01-04
+
+SCRIPT_DIR=`dirname "${0}"`
+MALT_DIR="${SCRIPT_DIR}/../build/maltparser"
+MALT_JAR="${MALT_DIR}/maltparser.jar"
+
+# Malt configuration file, for some absurd reason it needs to be in the
+#   current working directory, so create it there. It also creates a directory
+#   with a similar name! Depending on the `-m` argument.
+MCO=`mktemp tmp_XXXXXXXXXX.mco`
+MCO_DIR=`echo "${MCO}" | sed -e 's|\.mco$||g'`
+trap "rm -r -f ${MCO} ${MCO_DIR}" INT TERM EXIT
+
+# Make the conversion, Malt is noisy, so redirect stderr.
+java -jar "${MALT_JAR}" -c "${MCO}" -m proj -pp baseline -i /dev/stdin \
+    -o /dev/stdout 2> /dev/null
